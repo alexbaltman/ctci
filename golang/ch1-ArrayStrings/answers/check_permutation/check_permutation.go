953ad06c98d1159ddf82c0f123b34978dfc0b6b6
Alex Altman <aaltman@cisco.com>
2017-01-10 17:52:11 -0500

    Re-laying out the repo.

--- golang/ch1-ArrayStrings/answers/check_permutation/check_permutation.go
+++
@@ -1,8 +0,0 @@
-/*
-Given two strings, write a method to decide if one is a permutation of the other
-Hints: 1, 84, 122, 131
-*/
-package check_permutation
-
-func Check_Permutation(s1, s2 string) bool {
-}
--- golang/ch1-ArrayStrings/questions/isuniq.go
+++
@@ -1,8 +0,0 @@
-/*
-Implement an algo to determine if a string has all unique characters. What if you cannot use additional data structures?
-Hints: 44, 117, 132
-*/
-package isuniq
-
-func IsUniq() {
-}
